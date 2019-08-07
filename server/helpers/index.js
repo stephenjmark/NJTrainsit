8515f2cd3dfef7035b6246462c30e0fabe2d2ba2
Stephen Mark <stephenjmark@gmail.com>
2019-08-07 18:16:06 -0400

    getTrains returns a max of 5 trains

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -4,19 +4,29 @@ module.exports = {
     let result = [];
     console.log(trains);
 
-    trains.forEach(train => {
+    //iterate through trains
+
+    for (let i = 0; i < trains.length; i++) {
+      let train = trains[i];
       let stops = train["STOPS"]["STOP"];
-      stops.forEach(stop => {
+
+      //find which trains stop at desired destination
+      for (let j = 0; j < stops.length; j++) {
+        let stop = stops[j];
         let name = stop["NAME"];
         if (name === "Newark Penn Station") {
           result.push({
             depart: train["SCHED_DEP_DATE"],
             arrive: stop["TIME"]
           });
-        }
-      });
-    });
 
+          //return first 5 trains
+          if (result.length === 5) return result;
+        }
+      }
+    }
+
+    //return 0-4 trains
     return result;
   }
 };
